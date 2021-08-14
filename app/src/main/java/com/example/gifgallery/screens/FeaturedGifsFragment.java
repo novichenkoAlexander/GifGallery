faeb581b4d5ba3a354741eaa33fc44fe4f5d328a
novichenkoAlexander <a.novichenko1996@gmail.com>
2021-08-14 17:37:45 +0300

    naviagtion is set

---
+++ app/src/main/java/com/example/gifgallery/screens/FeaturedGifsFragment.java
@@ -0,0 +1,27 @@
+package com.example.gifgallery.screens;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.gifgallery.R;
+
+public class FeaturedGifsFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.featured_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
